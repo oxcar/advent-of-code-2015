e3bc26846e075e1e8b873253164396d75be9166f
Oscar Hernandez <oxcarh@gmail.com>
2016-12-19 19:55:41 -0600

    Day 3

--- src/main/scala/com/oxcarh/adventofcode2015/Day3.scala
+++ src/main/scala/com/oxcarh/adventofcode2015/Day3.scala
@@ -1,7 +1,6 @@
 package com.oxcarh.adventofcode2015
 
 import scala.collection._
-import java.util.concurrent.ConcurrentHashMap
 
 /*
 Day 3: Perfectly Spherical Houses in a Vacuum
@@ -12,17 +11,25 @@ object Day3 extends App {
 
   val input = Tools.loadDayInputAsText(day = 3)
 
+  // ----------------------------------------------------------------------
   // Solution 1
+  // ----------------------------------------------------------------------
+
   val houses = new mutable.HashMap[(Int, Int), Int]()
   var position = (0, 0)
   houses.put(position, 1)
   input.foreach { order =>
     val move = orderToMove(order)
     position = (position._1 + move._1, position._2 + move._2)
-    if(houses.contains(position)) houses.update(position, houses(position) + 1)
+    if (houses.contains(position)) houses.update(position, houses(position) + 1)
     else houses.put(position, 1)
   }
   println(s"Solution 1: ${houses.size}")
+
+  // ----------------------------------------------------------------------
+  // Solution 2
+  // ----------------------------------------------------------------------
+
 
   // ----------------------------------------------------------------------
 
